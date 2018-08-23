930b48efee1579e2cee109837c813059cca97741
lavinia-adochitei <lavinia.adochitei@yahoo.com>
2018-08-23 18:38:01 +0300

    Style for Not found page.

--- short-lnk/imports/ui/pages/NotFound.jsx
+++ short-lnk/imports/ui/pages/NotFound.jsx
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 
+import './NotFound.scss';
+
 class NotFound extends Component {
-    render() {
-        return <p>404 Not Found!</p>;
+    render() { 
+        return (
+            <div>
+              <section className="error-container">
+                <span><span>4</span></span>
+                <span>0</span>
+                <span><span>4</span></span>
+              </section>
+              <section className="not-found">
+                <span className="not">Not </span>
+                <span className="found">Found!</span>
+              </section>
+            </div>
+        );
     }
 }
 
